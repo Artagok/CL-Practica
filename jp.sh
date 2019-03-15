ae716068f2471d60a2e1b08af74ab8295a026a33
Pau Núñez <linkinpau.97@gmail.com>
2019-03-15 10:30:46 +0100

    Added verbose mode ./jp.sh -v that prints useful outputs

--- jp.sh
+++ jp.sh
@@ -5,6 +5,7 @@ _out=".out"
 _t=".t"
 _err=".err"
 _asl=".asl"
+separator="=============================="
 
 echo "It is assumed you are running the script inside asl folder, paths are relative"
 select fitxer in $(ls $ruta | grep $_asl);
@@ -13,7 +14,18 @@ do
     nom_fitxer=${fitxer%.asl} 
         
     [[ -e asl ]] || { echo "asl executable doesn't exist, please make it" && exit 1; }
+
     [[ $nom_fitxer =~ chkt ]] &&  ./asl $ruta$fitxer | egrep ^L > out.temp && diff $ruta$nom_fitxer$_err out.temp
     [[ $nom_fitxer =~ genc ]] && echo "GENC"
+
+    # Verbose mode
+    if [[ $# -gt 0 && $1 == "-v" ]]
+    then
+        echo ".ASL:"; cat -n $ruta$fitxer
+        echo; echo $separator; echo;
+        echo ".ERR:"; cat $ruta$nom_fitxer$_err
+        echo; echo $separator; echo;
+        echo ".TEMP:"; cat out.temp;
+    fi
     rm -f out.temp
 done
